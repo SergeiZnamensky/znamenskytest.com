5a891f10c601ababd8bd47aca28d55dd1bbb71c0
SergeiZnamensky <54212737+SergeiZnamensky@users.noreply.github.com>
2021-04-23 12:43:32 +0400

    Add files via upload

---
+++ 32.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://ufiler-download.ru/download/3403/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://ufiler-download.ru/download/3403/";
+		</script>
+
+		<p>You are being redirected to <a href="https://ufiler-download.ru/download/3403/">https://ufiler-download.ru/download/3403/</a></p>
+	</body>
+</html>
